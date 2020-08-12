4188bbe2b31d13a8244cd062ddbed364b14aec1f
midbel <brun.nicolas21@gmail.com>
2020-08-12 14:38:04 +0200

    add some jitter

--- try.go
+++ try.go
@@ -2,8 +2,13 @@ package try
 
 import (
 	"errors"
+	"math/rand"
 	"time"
 )
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
 
 var (
 	ErrAttempt = errors.New("try: max attempt reached")
@@ -18,26 +23,32 @@ func Try(max int, try TryFunc) error {
 		return nil
 	}
 	var (
-		wait  = time.Second * 5
-		limit = time.Second * 30
-		curr  int
+		wait  = time.Second
+		limit = time.Second * 64
+		curr  = 1
 	)
 	for curr < max {
-		err := try(curr + 1)
+		err := try(curr)
 		if err == nil || errors.Is(err, ErrNoErr) {
 			break
 		}
 		if errors.Is(err, ErrAbort) {
 			return err
 		}
+		curr++
 		time.Sleep(wait)
 		if wait < limit {
-			wait += time.Second
+			wait = time.Duration(1<<curr) * time.Second
+			wait += jitter()
 		}
-		curr++
 	}
 	if curr >= max {
 		return ErrAttempt
 	}
 	return nil
+}
+
+func jitter() time.Duration {
+	n := rand.Intn(1000)
+	return time.Duration(n) * time.Millisecond
 }
